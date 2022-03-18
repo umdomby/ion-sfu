af5367a224bf65df5733c4d4ff202eb1bf47dc10
navatar <umdomby@yandex.ru>
2022-03-18 18:20:34 +0300

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps : [{
+    name   : "video",
+    script  : "./main",
+    //watch: true,
+    //watch_delay: 1000,
+    //ignore_watch : ["node_modules"],
+    max_memory_restart: '700M',
+    // instances : "max",
+    // exec_mode : "cluster"
+    // stop_exit_codes: [0],
+    // exp_backoff_restart_delay: 100
+  }]
+}
